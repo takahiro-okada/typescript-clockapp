556e2e8b422c659068fc0eb87b8630eb86662df1
takahiro-okada <okahanako09875@gmail.com>
2023-01-01 04:38:10 +0900

    feat:Add animation for drawer menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import RegisterClock from './components/pages/RegisterClock';
--- src/components/pages/ClockPlace.tsx
+++ src/components/pages/ClockPlace.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as HeartIcon } from '../../images/icon-heart.svg';
+import ClockCard from '../Organisms/ClockCard';
 
 const ClockPlace = () => {
   console.log('hoge');
@@ -7,35 +7,11 @@ const ClockPlace = () => {
     <section className="mt-9 text-left container mx-auto px-3 py-6">
       <h2 className="font-bold text-3xl inline-block">スモールワールドのある場所のリスト</h2>
       <ul className="mt-6 grid grid-cols-2 gap-5">
-        <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl">
-          <h3>パワードーム</h3>
-          <div className="">愛知県半田市</div>
-          <div className="">
-            <img src="" alt="" />
-          </div>
-          <div className="">Small World</div>
-          <HeartIcon />
-        </li>
-
-        <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl">
-          <h3>パワードーム</h3>
-          <div className="">愛知県半田市</div>
-          <div className="">
-            <img src="" alt="" />
-          </div>
-          <div className="">Small World</div>
-          <HeartIcon />
-        </li>
-
-        <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl">
-          <h3>パワードーム</h3>
-          <div className="">愛知県半田市</div>
-          <div className="">
-            <img src="" alt="" />
-          </div>
-          <div className="">Small World</div>
-          <HeartIcon />
-        </li>
+        <ClockCard imgUrl="/jpg-smallworld.jpg" />
+        <ClockCard imgUrl="/jpg-smallworld.jpg" />
+        <ClockCard imgUrl="/jpg-smallworld.jpg" />
+        <ClockCard imgUrl="/jpg-smallworld.jpg" />
+        <ClockCard imgUrl="/jpg-smallworld.jpg" />
       </ul>
     </section>
   );
