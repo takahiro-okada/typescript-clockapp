d52a669225ef6f437d153dc155f4ca81425e3c57
takahiro-okada <okahanako09875@gmail.com>
2023-01-09 16:07:00 +0900

    feat:Add GET methos for maker

--- src/components/Organisms/MakerAndCategoryCard.tsx
+++ src/components/Organisms/MakerAndCategoryCard.tsx
@@ -1,9 +1,9 @@
 const MakerAndCategoryCard = (props: any) => {
-  const { name, imgUrl, url } = props;
+  const { maker, imgUrl } = props;
   return (
     <li className="px-4 py-4 shadow-lg shadow-gray-300 rounded-2xl">
-      <a href={url}>
-        <h3 className="text-xl font-extrabold">{name}</h3>
+      <a href={maker.makerName}>
+        <h3 className="text-xl font-extrabold">{maker.makerName}</h3>
         <div className="mt-3">
           <img src={imgUrl} alt="スモールワールド" className="w-full aspect-square rounded-2xl object-contain" />
         </div>
--- src/components/pages/MakerList.tsx
+++ src/components/pages/MakerList.tsx
@@ -1,13 +1,26 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import MakerAndCategoryCard from '../Organisms/MakerAndCategoryCard';
 
-const MakerList = () => (
-  <section className="mt-9 text-left container mx-auto px-3 py-6">
-    <h2 className="font-bold text-3xl">時計のメーカーリスト</h2>
-    <ul className="mt-6 grid grid-cols-2 gap-5">
-      <MakerAndCategoryCard url="/makers/citizen" name="シティズン" imgUrl="/logo-citizen.png" />
-      <MakerAndCategoryCard url="/makers/seiko" name="セイコー" imgUrl="/logo-seiko.jpeg" />
-    </ul>
-  </section>
-);
+const MakerList = () => {
+  const [makers, setMakers] = useState([]);
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/maker')
+      .then((response) => setMakers(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(makers);
 
+  return (
+    <section className="mt-9 text-left container mx-auto px-3 py-6">
+      <h2 className="font-bold text-3xl">時計のメーカーリスト</h2>
+      <ul className="mt-6 grid grid-cols-2 gap-5">
+        {makers.map((maker) => (
+          <MakerAndCategoryCard maker={maker} imgUrl="/logo-citizen.png" />
+        ))}
+      </ul>
+    </section>
+  );
+};
 export default MakerList;
