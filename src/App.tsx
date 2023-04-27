1ad0592d8394ead1a4f4663eb0ba31d5046ab6c4
takahiro-okada <okahanako09875@gmail.com>
2023-04-28 07:22:01 +0900

    feat: Modify register clock

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import RegisterClock from './components/pages/RegisterClock';
 import UsersAll from './components/pages/UsersAll';
-import RegisterPlace from './components/pages/RegisterPlace';
 import Footer from './components/Organisms/Footer';
 import Header from './components/Organisms/Header';
-import RegisterMaker from './components/pages/RegisterMaker';
 import PrefectureList from './components/pages/PrefectureList';
 import MakerList from './components/pages/MakerList';
 import MakerDetails from './components/pages/MakerDetails';
-import ClockPlace from './components/pages/ClockPlace';
 import CityList from './components/pages/CityList';
 import FavoriteList from './components/pages/FavoriteList';
 
@@ -21,15 +18,11 @@ const App = () => (
         <Route path="/" element={<Home />} />
         <Route path="/prefecture" element={<PrefectureList />} />
         <Route path="/prefecture/:prefecturename" element={<CityList />} />
-        <Route path="/prefecture/:prefecturename/:cityname" element={<ClockPlace />} />
         <Route path="/makers/" element={<MakerList />} />
         <Route path="/makers/:makername" element={<MakerDetails />} />
-        <Route path="/makers/:makername/:clockname" element={<ClockPlace />} />
         <Route path="/favorite" element={<FavoriteList />} />
         {/* admin */}
         <Route path="/register-clock" element={<RegisterClock />} />
-        <Route path="/register-place" element={<RegisterPlace />} />
-        <Route path="/register-maker" element={<RegisterMaker />} />
         <Route path="/users" element={<UsersAll />} />
       </Routes>
     </main>
